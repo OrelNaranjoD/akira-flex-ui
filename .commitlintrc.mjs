d059173081a28737bd4bedd2e1d84bc6d9a61ac9
Orel Naranjo <orelnaranjo@gmail.com>
2025-09-03 08:53:49 -0400

    chore: configure commitlint to ignore automated release commits

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -7,5 +7,10 @@ export default {
       'always',
       ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style', 'perf'],
     ],
+    'subject-empty': [0],
+    'type-empty': [0],
   },
+  ignores: [
+    (commit) => commit.includes('Release v') || commit.includes('[skip ci]'),
+  ],
 };
